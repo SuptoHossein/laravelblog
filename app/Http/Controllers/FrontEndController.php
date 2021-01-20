1ab054aa1c631ec4924286df2df81f3b4176834d
SuptoHossein <supto.hossein@gmail.com>
2021-01-20 16:40:55 +0600

    complete home page for frontend

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -9,22 +9,40 @@ class FrontEndController extends Controller
 {
     public function home()
     {
-        $post = Post::orderBy('created_at', 'DESC')->take(5)->get();
+        $posts = Post::with('category', 'user')->orderBy('created_at', 'ASC')->take(5)->get();
+
+        // posts splice for header
+        $firstPosts = $posts->splice(0, 2);
+        $middlePosts = $posts->splice(0, 1);
+        $lastPosts = $posts->splice(0);
+
+        // posts splice for footer
+        $footerPosts = Post::with('category', 'user')->orderBy('created_at', 'ASC')->inRandomOrder()->limit(4)->get();
+        $firstFooterPost = $footerPosts->splice(0, 1);
+        $middleFooterPost = $footerPosts->splice(0, 1);
+        $lastFooterPost = $footerPosts->splice(0, 2);
+        // return $firstFooterPost;
+
+        // Resent posts query here
         $recentPost = Post::with('category', 'user')->orderBy('created_at', 'DESC')->paginate(9);
-        return view('website.home', compact(['post', 'recentPost']));
+        return view('website.home', compact(['posts', 'recentPost', 'firstPosts', 'middlePosts', 'lastPosts', 'firstFooterPost', 'middleFooterPost', 'lastFooterPost', 'footerPosts']));
     }
+
     public function about()
     {
         return view('website.about');
     }
+
     public function category()
     {
         return view('website.category');
     }
+
     public function contact()
     {
         return view('website.contact');
     }
+
     public function post($slug)
     {
         $post = Post::with('category', 'user')->where('slug', $slug)->first();
@@ -35,6 +53,7 @@ class FrontEndController extends Controller
             return redirect('/');
         }
     }
+
     public function tag()
     {
         return view('website.tag');
